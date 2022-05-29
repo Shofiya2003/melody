a40f9b31502b26d0ac7287ad4f46a12606bbff04
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-29 18:26:30 +0530

    initialized npm and setup prettier and dotenv

---
+++ utils/logger.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+
+/*
+const winstonLevels = {
+error: 0,
+warn: 1,
+info: 2
+};
+*/
+
+module.exports = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
+});
