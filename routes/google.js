e5c332a1394315f2eba2bb815e576d12357f4a94
Shofiya2003 <shofiyabootwala@gmail.com>
2022-11-27 17:04:20 +0530

    added logic to add new user to database after google auth to google signin route

--- routes/google.js
+++ routes/google.js
@@ -1,22 +1,42 @@
-const express=require('express');
-const router=express.Router();
-const logger=require('../utils/logger/logger')
+const express = require('express');
+const router = express.Router();
+const logger = require('../utils/logger/logger');
+
+//user db
+const User = require("../models/user");
 
 //get google object
-const google=require('../utils/google/google');
+const google = require('../utils/google/google');
 
 google.seed();
 
 //function to take user to consent screen
-router.get('/signin/google',(req,res)=>{
-    const consentScreenUrl=google.generateUrl();
+router.get('/signin/google', (req, res) => {
+    const consentScreenUrl = google.generateUrl();
     res.redirect(consentScreenUrl);
 })
 
 //callback url
-router.get('/google/melody',google.exchangeCode,(req,res)=>{
-    logger.info('inside the call back url');
-    res.send('done');
+router.get('/google/melody', google.exchangeCode, async (req, res) => {
+    try {
+        logger.info('inside the call back url');
+        const user = JSON.parse(req.user);
+
+        //find the user in the database
+        const data = await User.findOne({ email: user["email"] });
+
+        if (!data) {
+            logger.info("new user");
+            const newUser = new User({ name: user.name, email: user["email"], googleId: user.id, picture: user.picture });
+            await newUser.save();
+        } else {
+            logger.info("user already registered");
+        }
+        res.json({ msg: "success", user: { email: user.email, name: user.name } });
+    } catch (err) {
+        logger.error(`something went wrong with logic ${err.msg}`);
+        res.json({ msg: "something went wrong" });
+    }
 })
 
-module.exports=router;
+module.exports = router;
