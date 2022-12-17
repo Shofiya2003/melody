6c336190ca42b084837f3c9488e172b2ea071619
Shofiya2003 <shofiyabootwala@gmail.com>
2022-12-18 00:48:49 +0530

    added methods to intialize the variables for spotify api and method to generate consent screen url for spotify

--- utils/spotify/spotify.js
+++ utils/spotify/spotify.js
@@ -1,2 +1,17 @@
+require('dotenv').config();
+
 function Spotify() { }
+
+Spotify.prototype.seed = () => {
+    this.clientId = process.env.SPOTIFY_CLIENT_ID;
+    this.clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+    this.redirectUri = process.env.SPOTIFY_REDIRECT_URL;
+}
+
+Spotify.prototype.generateUrl = () => {
+    const scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private';
+    const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${this.clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+    return url;
+}
+
 module.exports = new Spotify();
